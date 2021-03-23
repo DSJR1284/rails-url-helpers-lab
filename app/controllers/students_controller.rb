f97a761cde823130666bf02a9b021b041f62eee1
Doug Jones <DSJR1284@GMAIL.COM>
2021-03-22 20:36:17 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,8 @@ class StudentsController < ApplicationController
   def activate
     set_student
     @student.active = !@student.active
-    @student.save 
+    @student.save     
+    redirect_to student_path(@student)
   end 
 
   private
